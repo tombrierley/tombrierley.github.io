393571199a10168c4f933ec093ced690bd746548
tombrierley <54099609+tombrierley@users.noreply.github.com>
2020-08-17 08:13:25 +0000

    Deploying to master from  @ 5f4548cb5ab1f4ab98d9104f2c82d8deae5892b5 🚀

---
+++ _next/static/ZESTI99hcn4rJwQkikhx-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a],"/work":[a]}}("static\u002Fchunks\u002Fded88b54935de33fc29b0bbd101a108d176dd6da.73b0633b45f06761932a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
